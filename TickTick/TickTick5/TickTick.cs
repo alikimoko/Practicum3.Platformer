3a7d0e2491a1fa91bee569625603c627a0aaa1e3
Alex Vermeer <alex.vrmr@gmail.com>
2015-10-21 15:38:37 +0200

    Updated to actual exercize
    TODO: fix namespaces

---
+++ TickTick/TickTick5/TickTick.cs
@@ -0,0 +1,35 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Media;
+
+class TickTick : GameEnvironment
+{
+    static void Main()
+    {
+        TickTick game = new TickTick();
+        game.Run();
+    }
+
+    public TickTick()
+    {
+        Content.RootDirectory = "Content";
+        this.IsMouseVisible = true;
+    }
+
+    protected override void LoadContent()
+    {
+        base.LoadContent();
+
+        screen = new Point(1440, 825);
+        this.SetFullScreen(false);
+
+        gameStateManager.AddGameState("titleMenu", new TitleMenuState());
+        gameStateManager.AddGameState("helpState", new HelpState());
+        gameStateManager.AddGameState("playingState", new PlayingState(Content));
+        gameStateManager.AddGameState("levelMenu", new LevelMenuState());
+        gameStateManager.AddGameState("gameOverState", new GameOverState());
+        gameStateManager.AddGameState("levelFinishedState", new LevelFinishedState());
+        gameStateManager.SwitchTo("titleMenu");
+
+        AssetManager.PlayMusic("Sounds/snd_music");
+    }
+}
---
+++ TickTick/TickTick5/gameobjects/WaterDrop.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+class WaterDrop : SpriteGameObject
+{
+    protected float bounce;
+
+    public WaterDrop(int layer=0, string id="") : base("Sprites/spr_water", layer, id) 
+    {
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        double t = gameTime.TotalGameTime.TotalSeconds * 3.0f + Position.X;
+        bounce = (float)Math.Sin(t) * 0.2f;
+        position.Y += bounce;
+        Player player = GameWorld.Find("player") as Player;
+        if (this.visible && this.CollidesWith(player))
+        {
+            this.visible = false;
+            GameEnvironment.AssetManager.PlaySound("Sounds/snd_watercollected");
+        }
+    }
+}
