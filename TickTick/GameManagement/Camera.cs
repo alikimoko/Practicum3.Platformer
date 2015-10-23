a1712b5b47336e9189a4f63ad5a118266b37d2a6
SombreroZonderC <Rik.devendt@gmail.com>
2015-10-23 14:03:37 +0200

    Camera boundary's
    Added the possibility to check for boundary's, which will be implemented
    shortly

--- TickTick/GameManagement/Camera.cs
+++ TickTick/GameManagement/Camera.cs
@@ -14,38 +14,51 @@ namespace GameManagement
         Vector2 position;
         Rectangle cameraOutline, movingRectangle;
         bool moveVertical;
-        int moveableWidth = 200, moveableHeight = 200;
+        int moveableWidth = 200, moveableHeight = 200; //placeholder, subject to change
+        int rightBoundary, lowerBoundary;
         
-        public Camera(int screenWidth, int screenHeight, Vector2 startingPosition, bool moveVertical = false)
+        public Camera(int screenWidth, int screenHeight, Vector2 startingPosition, int levelWidth, int levelHeigth, bool moveVertical = false)
         {
             this.moveVertical = moveVertical;
             position = startingPosition;
+            calculateBoundary(levelWidth, levelHeigth);
+
             cameraOutline = new Rectangle((int)position.X, (int)position.Y, screenWidth, screenHeight);
             movingRectangle = new Rectangle((int)(position.X + (cameraOutline.Width/2 - moveableWidth/2)), (int)(position.Y + (cameraOutline.Height/2 - moveableHeight/2)) , moveableWidth , moveableHeight );
         }
 
-        public Camera(int screenWidth, int screenHeight, Vector2 startingPosition, int moveableHeight, int moveableWidth, bool moveVertical = false)
+        public Camera(int screenWidth, int screenHeight, Vector2 startingPosition, int levelWidth, int levelHeigth, int moveableHeight, int moveableWidth, bool moveVertical = false)
         {
             this.moveVertical = moveVertical;
             position = startingPosition;
             this.moveableWidth = moveableWidth;
             this.moveableHeight = moveableHeight;
+
+            
             cameraOutline = new Rectangle((int)position.X, (int)position.Y, screenWidth, screenHeight);
             movingRectangle = new Rectangle((int)(position.X + (cameraOutline.Width / 2 - moveableWidth / 2)), (int)(position.Y + (cameraOutline.Height / 2 - moveableHeight / 2)), this.moveableWidth, this.moveableHeight);
         }
+
+        private void calculateBoundary(int levelWidth, int levelHeigth)
+        {
+            rightBoundary = levelWidth - cameraOutline.Width;
+            lowerBoundary = levelHeigth - cameraOutline.Height;
+        }
+
         public bool MoveVertical { get { return moveVertical; }}
         public Vector2 Position { get { return position; }
             set
             {
                 if (value.X >= 0) { 
-                position = value; 
+                position.X = value.X; 
                 cameraOutline.X = (int)value.X; 
                 movingRectangle.X= (int) (position.X +(cameraOutline.Width/2 - moveableWidth/2));
-                if (MoveVertical)
-                {
-                    cameraOutline.Y = (int)value.Y;
-                    movingRectangle.Y = (int)(position.Y + (cameraOutline.Height / 2 - moveableHeight / 2));
                 }
+                if (MoveVertical && value.Y>=0)
+                {
+                position.Y = value.Y;
+                cameraOutline.Y = (int)value.Y;
+                movingRectangle.Y = (int)(position.Y + (cameraOutline.Height / 2 - moveableHeight / 2));
                 }
             }
         }
