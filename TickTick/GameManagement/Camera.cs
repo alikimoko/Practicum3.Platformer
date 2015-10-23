8a28d6fb182fef97de375b3869e0afe84d2b8c55
SombreroZonderC <Rik.devendt@gmail.com>
2015-10-23 13:44:22 +0200

    Camera set and get position
    get and set the position of the camera with one simple variable change.

--- TickTick/GameManagement/Camera.cs
+++ TickTick/GameManagement/Camera.cs
@@ -21,7 +21,7 @@ namespace GameManagement
             this.moveVertical = moveVertical;
             position = startingPosition;
             cameraOutline = new Rectangle((int)position.X, (int)position.Y, screenWidth, screenHeight);
-            movingRectangle = new Rectangle((cameraOutline.Width/2 - moveableWidth/2), (cameraOutline.Height/2 - moveableHeight/2) , moveableWidth , moveableHeight );
+            movingRectangle = new Rectangle((int)(position.X + (cameraOutline.Width/2 - moveableWidth/2)), (int)(position.Y + (cameraOutline.Height/2 - moveableHeight/2)) , moveableWidth , moveableHeight );
         }
 
         public Camera(int screenWidth, int screenHeight, Vector2 startingPosition, int moveableHeight, int moveableWidth, bool moveVertical = false)
@@ -31,8 +31,26 @@ namespace GameManagement
             this.moveableWidth = moveableWidth;
             this.moveableHeight = moveableHeight;
             cameraOutline = new Rectangle((int)position.X, (int)position.Y, screenWidth, screenHeight);
-            movingRectangle = new Rectangle((cameraOutline.Width / 2 - this.moveableWidth / 2), (cameraOutline.Height / 2 - this.moveableHeight / 2), this.moveableWidth, this.moveableHeight);
+            movingRectangle = new Rectangle((int)(position.X + (cameraOutline.Width / 2 - moveableWidth / 2)), (int)(position.Y + (cameraOutline.Height / 2 - moveableHeight / 2)), this.moveableWidth, this.moveableHeight);
         }
-            
+        public bool MoveVertical { get { return moveVertical; }}
+        public Vector2 Position { get { return position; }
+            set
+            {
+                if (value.X >= 0) { 
+                position = value; 
+                cameraOutline.X = (int)value.X; 
+                movingRectangle.X= (int) (position.X +(cameraOutline.Width/2 - moveableWidth/2));
+                if (MoveVertical)
+                {
+                    cameraOutline.Y = (int)value.Y;
+                    movingRectangle.Y = (int)(position.Y + (cameraOutline.Height / 2 - moveableHeight / 2));
+                }
+                }
+            }
+        }
+
+        
+
     }
 }
